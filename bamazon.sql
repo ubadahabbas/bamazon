7a2b0ba84ba5b6e3faf701b0fc503d95dbd41a10
Ubadah Atta <ubadahatta@Ubadahs-MacBook.fios-router.home>
2019-01-13 21:01:33 -0500

    challenge #1

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+ product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("The Alchemist", "Novels", 30,250);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("The Power of Habit", "Self Help", 25,200);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Thinking Fast and Slow", "Self Help", 20,200);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Origin", "Thriller", 40,350);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("The Hobbit", "Novels", 20,150);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Season of Migration to the North", "Fiction", 35,250);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("The Wedding of Zein", "Fiction", 20,150);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Khartoum at Night", "History", 15,50);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Milk and honey", "Poetry", 15, 90);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Seriously...I'm Kidding", "Humer", 30,250);
---
+++ bamazonCustomer.js
@@ -0,0 +1,83 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer")
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazon_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        for (var i = 0; i < results.length; i++) {
+            console.log("Product ID: " + results[i].id)
+            console.log("Product Name: " + results[i].product_name)
+            console.log("Price: " + results[i].price)
+            console.log("--------------------------------------------")
+        }
+        inquirer.prompt([{
+            name: "choice",
+            type: "rawlist",
+            choices: function () {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    choiceArray.push(results[i].product_name);
+                }
+                return choiceArray;
+            },
+            message: "Which product would you like to purchase?"
+        },
+        {
+            name: "units",
+            type: "input",
+            message: "How many units?"
+        }
+        ]).then(function (answer) {
+            var productID;
+            for (var i = 0; i < results.length; i++) {
+                if (answer.choice == results[i].product_name) {
+                    productID = results[i].id;
+                    if (answer.units <= results[i].stock_quantity) {
+                        var updatedQuantity = results[i].stock_quantity - answer.units;
+                        var totalCost = answer.units * results[i].price;
+                        connection.query("UPDATE products SET ? WHERE ?",
+                            [
+                                { stock_quantity: updatedQuantity },
+                                { id: productID }
+                            ],
+                            function (error) {
+                                if (error) throw err;
+                                console.log("------------------------------------------")
+                                console.log("Order placed successfully");
+                                console.log("Order Summary  --->");
+                                console.log("Product: " + answer.choice);
+                                console.log("Quantity: "+ answer.units);
+                                console.log("Total: $"+ totalCost);
+                                console.log("------------------------------------------")
+                            }
+                        );
+                    } else {
+                        console.log("Quantity available is: " + results[i].stock_quantity);
+                    }
+                }
+            }
+        })
+    })
+}
